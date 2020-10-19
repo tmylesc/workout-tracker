c504a4c7fb7e4cdc9ee71d1947abcb9cbfefd999
Thomas Coradetti <tmcoradetti@gmail.com>
2020-10-18 20:45:13 -0400

    adding readme

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# workout-tracker
+# Fitness Tracker
+
+![app](https://raw.githubusercontent.com/tmylesc/workout-tracker/main/img/Screenshot.png)
+
+## Description
+
+This app lets a user view create and track daily workouts. The user can log multiple exercises in a workout on a given day. The user can also track the name, type, weight, sets, reps, and duration of exercise. If the exercise is a cardio exercise, the user can track the distance traveled.
+
+## Deployment 
+
+This App is deployed on Heroku: [https://workout-tracker-tmylesc.herokuapp.com/](https://workout-tracker-tmylesc.herokuapp.com/)
+
+## Technologies used
+
+- MongoDB
+- Express
+- Node
--- routes/api.js
+++ routes/api.js
@@ -1,58 +1,58 @@
-const router = require('express').Router();
-const Workout = require('../models/workout');
+const router = require("express").Router();
+const Workout = require("../models/workout.js");
 
 router.get("/api/workouts", (req, res) => {
-    Workout.find()
-      .then(workouts => {
-        res.json(workouts);
-      })
-      .catch(err => {
-        res.json(err);
-      });
-  });
+  Workout.find()
+    .then(dbWorkouts => {
+      res.json(dbWorkouts);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
 
-  router.get("/api/workouts/range", ({ query }, res) => {
-    Workout.find({ day: { $gte: query.start, $lte: query.end } })
-      .then(workouts => {
-        res.json(workouts);
-      })
-      .catch(err => {
-        res.json(err);
-      });
-  });
+router.get("/api/workouts/range", ({ query }, res) => {
+  Workout.find({ day: { $gte: query.start, $lte: query.end } })
+    .then(dbWorkouts => {
+      res.json(dbWorkouts);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
 
-router.post('/api/workouts', (req, res) => {
-    Workout.create({})
-        .then(workout => {
-            res.json(workout);
-        })
-        .catch(err => {
-            res.json(err);
-        });
-});  
+router.post("/api/workouts", (req, res) => {
+  Workout.create({})
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
 
 router.put("/api/workouts/:id", ({ body, params }, res) => {
-    Workout.findByIdAndUpdate(
-      params.id,
-      { $push: { exercises: body } },
-      { new: true, runValidators: true }
-    )
-      .then(workout => {
-        res.json(workout);
-      })
-      .catch(err => {
-        res.json(err);
-      });
-  });
+  Workout.findByIdAndUpdate(
+    params.id,
+    { $push: { exercises: body } },
+    { new: true, runValidators: true }
+  )
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
 
-  router.delete("/api/workouts", ({ body }, res) => {
-    Workout.findByIdAndDelete(body.id)
-      .then(() => {
-        res.json(true);
-      })
-      .catch(err => {
-        res.json(err);
-      });
-  });
+router.delete("/api/workouts", ({ body }, res) => {
+  Workout.findByIdAndDelete(body.id)
+    .then(() => {
+      res.json(true);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
 
-  module.exports = router;
+module.exports = router;
